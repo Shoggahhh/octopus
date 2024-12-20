1c1ae4dbb6267d80ca831e6863fe28c8f79214a2
Shoggahhh <vass_stn@mail.ru>
2024-12-20 11:06:20 +0300

    add main, checkout to dev

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ from datetime import date
 
 from for_logger import log_this
 
-
 if __name__ == '__main__':
     log_this()
 
     livolo = Octopus("Livolo")
-    livolo.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс лист")
+    livolo.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс")
     livolo.transfer_to_ftp()
 
     rocket_socket = Octopus("Rocket_socket")
-    rocket_socket.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс лист")
+    rocket_socket.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс")
     rocket_socket.transfer_to_ftp()
+
+    welrok = Octopus("Welrok")
+    welrok.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс")
+    welrok.transfer_to_ftp()
 
     terneo = Octopus("Terneo")
     terneo.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс лист")
     terneo.transfer_to_ftp()
-
-
 
     # arlight = Octopus("Arlight")
     # arlight.auth_and_download_from_link(tag_login='loginUser',
@@ -37,13 +38,7 @@ if __name__ == '__main__':
     camelion.transfer_to_ftp()
 
     italline = Octopus("Italline")
-    italline.auth_and_download_from_link(tag_login='login',
-                                         tag_password='password',
-                                         login=OCTOPUS_SETTINGS.get('ITALLINE_LOGIN'),
-                                         password=OCTOPUS_SETTINGS.get('ITALLINE_PASSWORD'),
-                                         base_url='https://abrissvet.ru/webasyst/',
-                                         url_to_file='https://abrissvet.ru/webasyst/files/?module=file&action=download&id=341'
-                                         )
+    italline.get_file_from_yandex("https://disk.yandex.ru/i/r6i-TCZfuqr2qw", file_name="складские")
     italline.transfer_to_ftp()
 
     ultraflash = Octopus("Ultraflash")
@@ -67,15 +62,19 @@ if __name__ == '__main__':
     newport_price.transfer_to_ftp()
 
     simple_story = Octopus("Simple story")
-    simple_story.get_file_from_yandex('https://disk.yandex.ru/d/hDHeLMp3dW6TRw', 'действующий')
+    simple_story.get_file_from_yandex('https://disk.yandex.ru/d/hDHeLMp3dW6TRw', file_name='действующий')
 
     ovivo = Octopus("Ovivo")
     # ovivo.get_file_from_mailru("https://cloud.mail.ru/public/mQqu/XEoebtHtX", 'Остатки Овиво')
-    ovivo.get_file_from_yandex(url='https://disk.yandex.ru/d/_YU5JEZnN0oRZQ', file_name='Остатки Овиво')
+    ovivo.get_file_from_yandex(url='https://disk.yandex.ru/d/_YU5JEZnN0oRZQ', file_name='Остатки')
     ovivo.transfer_to_ftp()
 
+    bylectrica = Octopus("Bylectrica")
+    bylectrica.get_file_from_yandex(url='https://disk.yandex.ru/d/_YU5JEZnN0oRZQ', file_name='Остатки')
+    bylectrica.transfer_to_ftp()
+
     leek = Octopus("Leek")
-    leek.find_massage_from_outlook('1c8@energoco.ru', 'Остатки складов МФ с ценами')
+    leek.find_massage_from_outlook('1c8@energoco.ru', 'Остатки складов с ценами')
     leek.transfer_to_ftp()
 
     brizzy = Octopus("Brizzi")
@@ -85,6 +84,16 @@ if __name__ == '__main__':
     )
     brizzy.transfer_to_ftp()
 
+    apeyron = Octopus("Apeyron")
+    apeyron.get_file_from_link("http://storage.aeled.ru/feed_apeyron.yml")
+    apeyron.transfer_to_ftp()
+
     # kutek = Octopus("Kutek")
     # kutek.get_file_from_mailru(url='https://cloud.mail.ru/public/FoMy/4Hbh6fNra', name='наличие на')
     # kutek.transfer_to_ftp()
+
+    # sferon_brands = ["Прайс Arti Lampadari", "Прайс Dio D'Arte", "Прайс Lucia Tucci"]
+    # sferon = Octopus("Sferon")
+    # sferon.get_all_files_from_yandex("https://disk.yandex.ru/d/sa2zN832OAzAUQ", file_names=sferon_brands,
+    #                                  format_file='xlsx')
+    # sferon.transfer_to_ftp()
