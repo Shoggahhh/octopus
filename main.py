34b59447742425ad613bdb4566a6d3b5e5054626
grinev <vass_grinev@mail.ru>
2025-06-09 16:23:38 +0300

    add docstring

--- main.py
+++ main.py
@@ -1,92 +1,20 @@
 from octopus import Octopus
-from settings import OCTOPUS_SETTINGS
-from datetime import date
+from pathlib import Path
 
-from for_logger import log_this
 
-if __name__ == '__main__':
-    log_this()
+if __name__ == "__main__":
+    path_to_file = Path("files")
+    path_to_logs = Path("logs")
+    path_to_file.mkdir(exist_ok=True)
+    path_to_logs.mkdir(exist_ok=True)
 
-    livolo = Octopus("Livolo")
-    livolo.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс")
-    livolo.transfer_to_ftp()
-
-    rocket_socket = Octopus("Rocket_socket")
-    rocket_socket.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс")
-    rocket_socket.transfer_to_ftp()
-
-    welrok = Octopus("Welrok")
-    welrok.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс")
-    welrok.transfer_to_ftp()
-
-    terneo = Octopus("Terneo")
-    terneo.get_file_from_mailru("https://cloud.mail.ru/public/1ZWJ/4ncMnVjVa", "Прайс лист")
-    terneo.transfer_to_ftp()
-
-    # arlight = Octopus("Arlight")
-    # arlight.auth_and_download_from_link(tag_login='loginUser',
-    #                                     tag_password='loginPass',
-    #                                     login=OCTOPUS_SETTINGS.get('ARLIGHT_LOGIN'),
-    #                                     password=OCTOPUS_SETTINGS.get('ARLIGHT_PASSWORD'),
-    #                                     base_url='https://assets.transistor.ru/',
-    #                                     url_to_file='https://assets.transistor.ru/price/v3/sites/price.xlsx'
-    #                                     )
-    # arlight.transfer_to_ftp()
-
-    camelion = Octopus("Camelion")
-    camelion.find_massage_from_outlook('info@e-s-t.ru', f'Остатки товаров на {date.today().strftime("%d.%m.%Y")}')
-    camelion.transfer_to_ftp()
-
-    italline = Octopus("Italline")
-    italline.get_file_from_yandex("https://disk.yandex.ru/i/r6i-TCZfuqr2qw", file_name="складские")
-    italline.transfer_to_ftp()
-
-    ultraflash = Octopus("Ultraflash")
-    ultraflash.find_massage_from_outlook('info@e-s-t.ru', f'Остатки товаров на {date.today().strftime("%d.%m.%Y")}')
-    ultraflash.transfer_to_ftp()
-
-    newport_stock = Octopus("Newport_stocks")
-    newport_stock.get_file_from_link("http://178.177.4.229:8087/publ/Остатки NEWPORT.csv")
-    newport_stock.transfer_to_ftp()
-
-    newport_price = Octopus("Newport_price")
-    newport_price.get_file_from_link("https://newport-light.ru/Down/price-newport-mrc.xlsx")
-    newport_price.transfer_to_ftp()
-
-    simple_story = Octopus("Simple story")
-    simple_story.get_file_from_yandex('https://disk.yandex.ru/d/hDHeLMp3dW6TRw', file_name='действующий')
-
-    ovivo = Octopus("Ovivo")
-    ovivo.get_file_from_yandex(url='https://disk.yandex.ru/d/_YU5JEZnN0oRZQ', file_name='Остатки')
-    ovivo.transfer_to_ftp()
-
-    bylectrica = Octopus("Bylectrica")
-    bylectrica.get_file_from_yandex(url='https://disk.yandex.ru/d/_YU5JEZnN0oRZQ', file_name='Остатки')
-    bylectrica.transfer_to_ftp()
-
-    leek = Octopus("Leek")
-    leek.find_massage_from_outlook('1c8@energoco.ru', 'Остатки складов с ценами')
-    leek.transfer_to_ftp()
-
-    brizzy = Octopus("Brizzi")
-    brizzy.get_file_from_google(
-        url='https://docs.google.com/spreadsheets/u/0/d/1o3j5DucKsvX2gPJi21KsZB0sgQEN4J3CB-YzVtSy5YY/export',
-        format_file='xlsx'
+    """
+    Example:
+    
+    some_brand = Octopus("Some_brand")
+    some_brand.get_file_from_yandex(
+        "yandex_url",
+        "file_name_from_yandex",
+        format_file_xlsx="xlsx",
     )
-    brizzy.transfer_to_ftp()
-
-    apeyron = Octopus("Apeyron")
-    apeyron.get_file_from_link("http://storage.aeled.ru/feed_apeyron.yml")
-    apeyron.transfer_to_ftp()
-
-    arti_lampadari = Octopus("Arti Lampadari")
-    arti_lampadari.get_file_from_yandex("https://disk.yandex.ru/d/sa2zN832OAzAUQ", "Прайс Arti", "xlsx")
-    arti_lampadari.transfer_to_ftp()
-
-    dio_de_arte = Octopus("Dio D'Arte")
-    dio_de_arte.get_file_from_yandex("https://disk.yandex.ru/d/sa2zN832OAzAUQ", "Прайс Dio", "xlsx")
-    dio_de_arte.transfer_to_ftp()
-
-    lucia_tucci = Octopus("Lucia Tucci")
-    lucia_tucci.get_file_from_yandex("https://disk.yandex.ru/d/sa2zN832OAzAUQ", "Прайс Lucia", "xlsx")
-    lucia_tucci.transfer_to_ftp()
+    """
--- octopus.py
+++ octopus.py
@@ -1,78 +1,123 @@
 import ftplib
 import json
 import os
-import shutil
+
+# import shutil
 from datetime import date
 from urllib.parse import urlencode, quote
-from zipfile import ZipFile
+
+# from zipfile import ZipFile
 
 import requests
-from loguru import logger
-from settings import OCTOPUS_SETTINGS
+import logging
+from settings import settings
 
 import win32com.client as win32
+
+FORMAT = "%(asctime)s %(clientip)-15s %(user)-8s %(message)s"
+logging.basicConfig(format=FORMAT, filename="logs/logs.log", level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Octopus:
     def __init__(self, brand):
         self.BRAND = brand
-        self.FTP_HOST = '5.188.156.224'
-        self.FTP_USER = OCTOPUS_SETTINGS.get('FTP_USER')
-        self.FTP_PASSWORD = OCTOPUS_SETTINGS.get('FTP_PASSWORD')
-        self.FTP_PATH = '/input/b85f50c4-b985-11ec-a5fc-020017000b7b/'
-        self.PATH_TO_FILE = 'C:/Users/grinev.TL/PycharmProjects/octopus/files/'
+        self.FTP_HOST = settings.octopus_settings.ftp_host
+        self.FTP_USER = settings.octopus_settings.ftp_user
+        self.FTP_PASSWORD = settings.octopus_settings.ftp_password
+        self.FTP_PATH = settings.octopus_settings.ftp_path
+        self.PATH_TO_FILE = settings.octopus_settings.path_to_file
 
-    def auth_and_download_from_link(self, tag_login, tag_password, login, password, base_url, url_to_file):
-        logger.info(f'\n{self.BRAND}')
+    def auth_and_download_from_link(
+        self,
+        tag_login: str,
+        tag_password: str,
+        login: str,
+        password: str,
+        base_url: str,
+        url_to_file: str,
+    ) -> None:
+        """
+        :param tag_login: Имя поля, которое хранится в теге login
+        :param tag_password: Имя поля, которое хранится в теге password
+        :param login: Логин, который нужно написать в поле login
+        :param password: Пароль, который нужно написать в поле login
+        :param base_url: Страница, где требуется аутентификация
+        :param url_to_file: Ссылка до файла, который нужно скачать
+        :return:
+
+        Метод, если требуется скачать файл с сайта, где требуется аутентификация
+
+        """
+        logger.info(f"\n{self.BRAND}")
 
         session = requests.Session()
 
-        user = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0'
-        headers = {
-            'user-agent': user
-        }
+        user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0"
+        headers = {"user-agent": user}
 
         data = {
             tag_login: login,
             tag_password: password,
         }
         try:
-            logger.info('authorization...')
+            logger.info("authorization...")
 
             session.post(base_url, data=data, headers=headers)
 
-            logger.info('authorization completed')
+            logger.info("authorization completed")
 
-            logger.info('request to file')
+            logger.info("request to file")
 
             get_file = session.get(url_to_file, headers=headers)
 
-            with open(f'{self.PATH_TO_FILE}{self.BRAND}.xlsx', 'wb') as f:
+            with open(f"{self.PATH_TO_FILE}{self.BRAND}.xlsx", "wb") as f:
                 f.write(get_file.content)
 
-            logger.info(f'status code: {get_file.status_code} {self.BRAND}: download is complete')
+            logger.info(
+                f"status code: {get_file.status_code} {self.BRAND}: download is complete"
+            )
         except Exception as ex:
-            logger.error(f'{self.BRAND}: {ex}')
+            logger.error(f"{self.BRAND}: {ex}")
 
-    def get_file_from_link(self, url):
-        logger.info(f'\n{self.BRAND}')
-        logger.info('request to file')
+    def get_file_from_link(self, url: str) -> None:
+        """
+        :param url: Ссылка до файла
+        :return:
+        Метод для скачивания файла по прямой ссылке
+        """
+        logger.info(f"\n{self.BRAND}")
+        logger.info("request to file")
         try:
             get_file = requests.get(url)
 
-            with open(f'{self.PATH_TO_FILE}{self.BRAND}.{url.split(".")[-1]}', 'wb') as f:
+            with open(
+                f'{self.PATH_TO_FILE}{self.BRAND}.{url.split(".")[-1]}', "wb"
+            ) as f:
                 f.write(get_file.content)
 
-            logger.info(f'status code: {get_file.status_code} {self.BRAND}: download is complete')
+            logger.info(
+                f"status code: {get_file.status_code} {self.BRAND}: download is complete"
+            )
         except Exception as ex:
-            logger.error(f'{self.BRAND}: {ex}')
+            logger.error(f"{self.BRAND}: {ex}")
 
-    def find_massage_from_outlook(self, sender, theme_of_message):
-        logger.info(f'\n{self.BRAND}')
-        outlook = win32.Dispatch('Outlook.Application').GetNamespace('MAPI')
+    def find_massage_from_outlook(self, sender: str, theme_of_message: str) -> None:
+        """
+        :param sender:
+        :param theme_of_message:
+        :return:
+
+        Метод для скичавания файла с Outlook
+        Метод работает только на Windows для поиска файла на почте Outlook
+        Чтобы работать с web версией нужно зарегестрировать приложение на Azure
+
+        """
+        logger.info(f"\n{self.BRAND}")
+        outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
         main_folder = outlook.Folders.Item(2)
-        sub_folder = main_folder.Folders('Входящие').Folders('ОСТАТКИ')
+        sub_folder = main_folder.Folders("Входящие").Folders("ОСТАТКИ")
         # folder = outlook.GetDefaultFolder(6)
 
         messages = sub_folder.Items
@@ -80,40 +125,53 @@ class Octopus:
         today_now = date.today()
         try:
             for message in messages:
-                if message.SenderEmailAddress == sender and message.Subject == theme_of_message and message.ReceivedTime.date() == today_now:
-                    logger.info('Message found')
-                    logger.info('get attachments')
+                if (
+                    message.SenderEmailAddress == sender
+                    and message.Subject == theme_of_message
+                    and message.ReceivedTime.date() == today_now
+                ):
+                    logger.info("Message found")
+                    logger.info("get attachments")
                     try:
                         for att in message.Attachments:
-                            att.SaveAsFile(f'{self.PATH_TO_FILE}{self.BRAND}.{str(att).split(".")[-1]}')
-                            logger.info(f'{self.BRAND}: file saved')
+                            att.SaveAsFile(
+                                f'{self.PATH_TO_FILE}{self.BRAND}.{str(att).split(".")[-1]}'
+                            )
+                            logger.info(f"{self.BRAND}: file saved")
                     except Exception as ex:
-                        logger.error(f'{self.BRAND}: file not found')
+                        logger.error(f"{self.BRAND} - {ex}: file not found")
         except Exception as ex:
-            logger.error(f'{self.BRAND}: {ex}')
+            logger.error(f"{self.BRAND}: {ex}")
 
-    def get_file_from_mailru(self, url, name):
-        logger.info(f'\n{self.BRAND}')
+    def get_file_from_mailru(self, url: str, name: str) -> None:
+        """
+        :param url: Ссылка на файлы
+        :param name: Имя файла
+        :return:
+
+        Метод для скачивания файла с mail диск
+        """
+        logger.info(f"\n{self.BRAND}")
         id_mailru = f'{url.split("/")[-2]}/{url.split("/")[-1]}'
-        api_mailru = f'https://cloud.mail.ru/api/v4/public/list?weblink={id_mailru}'
+        api_mailru = f"https://cloud.mail.ru/api/v4/public/list?weblink={id_mailru}"
 
-        file_name = ''
+        file_name = ""
 
         try:
             response = requests.get(api_mailru)
             response.raise_for_status()
 
-            logger.info(f'status_code: {response.status_code}')
+            logger.info(f"status_code: {response.status_code}")
 
             response = response.text
             data = json.loads(response)
 
             check = False
-            for item in data['list']:
-                data_name = item['name']
+            for item in data["list"]:
+                data_name = item["name"]
                 if name.lower() in data_name.lower():
                     check = True
-                    file_name = item['weblink'].split('/')[-1]
+                    file_name = item["weblink"].split("/")[-1]
                     logger.info(f'"{name}" found')
                     break
 
@@ -127,33 +185,52 @@ class Octopus:
             temp = requests.get(url)
             temp.raise_for_status()
 
-            logger.info(f'status_code: {temp.status_code}')
+            logger.info(f"status_code: {temp.status_code}")
 
             temp = temp.text
 
-            link = temp.partition('"weblink_get":')[2].partition(',"weblink_thumbnails"')[0]
-            link = link.partition('"url":"')[2].partition('"')[0] + f'/{id_mailru}/{file_name}'
+            link = temp.partition('"weblink_get":')[2].partition(
+                ',"weblink_thumbnails"'
+            )[0]
+            link = (
+                link.partition('"url":"')[2].partition('"')[0]
+                + f"/{id_mailru}/{file_name}"
+            )
 
             file_xlsx = requests.get(link).content
 
-            with open(f'{self.PATH_TO_FILE}{self.BRAND}.{file_name.split(".")[-1]}', 'wb') as f:
+            with open(
+                f'{self.PATH_TO_FILE}{self.BRAND}.{file_name.split(".")[-1]}', "wb"
+            ) as f:
                 f.write(file_xlsx)
+                return None
 
         except requests.exceptions.HTTPError as ex:
             logger.error(ex)
+            return None
 
-    def get_file_from_yandex(self, url: str, file_name: str, format_file_xlsx: str = None):
-        logger.info(f'\n{self.BRAND}')
-        format_xlsx = ['xls', 'xlsx', 'csv']
+    def get_file_from_yandex(
+        self, url: str, file_name: str, format_file_xlsx: str = None
+    ) -> None:
+        """
+        :param url: Ссылка на диск
+        :param file_name: Имя файла на диске
+        :param format_file_xlsx: Формат файла нужно указать, если есть два файла с одинаковыми названиями
+        :return:
+
+        Метод для скачивания файла с yandex диск
+        """
+        logger.info(f"\n{self.BRAND}")
+        format_xlsx = ["xls", "xlsx", "csv"]
 
         if format_file_xlsx not in format_xlsx and format_file_xlsx is not None:
             logger.error("this format file is not excel")
             raise "this format file is not excel"
 
-        base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources?'
+        base_url = "https://cloud-api.yandex.net/v1/disk/public/resources?"
         params = {"public_key": url}
 
-        yandex_url = base_url + urlencode(query=params, safe='/', quote_via=quote)
+        yandex_url = base_url + urlencode(query=params, safe="/", quote_via=quote)
 
         try:
             response = requests.get(yandex_url)
@@ -166,60 +243,72 @@ class Octopus:
 
             for url_file in urls:
                 file = requests.get(url_file).content
-                path_to_file = f'{self.PATH_TO_FILE}{self.BRAND}.{format_file_xlsx}'
-                with open(path_to_file, 'wb') as f:
+                path_to_file = f"{self.PATH_TO_FILE}{self.BRAND}.{format_file_xlsx}"
+                with open(path_to_file, "wb") as f:
                     f.write(file)
 
         except Exception as ex:
             logger.error(ex)
 
-    def get_file_from_google(self, url, format_file):
-        logger.info(f'\n{self.BRAND}')
+    def get_file_from_google(self, url: str, format_file: str) -> None:
+        """
+        :param url: ссылка на google диск
+        :param format_file: формат файла, требуется указывать для params
+        :return:
+
+        Метод для скачивания файла с google диск
+        """
+        logger.info(f"\n{self.BRAND}")
         try:
             params = {
-                'format': f'{format_file}',
+                "format": f"{format_file}",
             }
             response = requests.get(url, params=params)
             response.raise_for_status()
 
-            logger.info(f'status_code: {response.status_code}')
+            logger.info(f"status_code: {response.status_code}")
 
-            with open(f'{self.PATH_TO_FILE}{self.BRAND}.{params.get("format")}', 'wb') as f:
+            with open(
+                f'{self.PATH_TO_FILE}{self.BRAND}.{params.get("format")}', "wb"
+            ) as f:
                 f.write(response.content)
         except Exception as ex:
             logger.error(ex)
 
-    def transfer_to_ftp(self):
+    def transfer_to_ftp(self) -> None:
+        """
+        Метод для отправки файла на FTP, откуда потом цены и остатки подхватит 1С
+        """
         ftp_server = ftplib.FTP(self.FTP_HOST, self.FTP_USER, self.FTP_PASSWORD)
-        ftp_server.encoding = 'utf-8'
+        ftp_server.encoding = "utf-8"
 
-        error_code = ''
+        error_code = ""
         try:
-            ftp_server.cwd(f'{self.FTP_PATH}{self.BRAND}')
+            ftp_server.cwd(f"{self.FTP_PATH}{self.BRAND}")
         except ftplib.all_errors as ex:
             error_code = str(ex).split(None, 1)[0]
             logger.error(ex)
 
         if error_code == "550":
             logger.info(f"create new directory: {self.FTP_PATH}{self.BRAND}")
-            ftp_server.mkd(f'{self.FTP_PATH}{self.BRAND}')
-            ftp_server.cwd(f'{self.FTP_PATH}{self.BRAND}')
+            ftp_server.mkd(f"{self.FTP_PATH}{self.BRAND}")
+            ftp_server.cwd(f"{self.FTP_PATH}{self.BRAND}")
 
-        path_to_file = ''
+        # path_to_file = ""
         files = []
         for file in os.listdir(self.PATH_TO_FILE):
-            if self.BRAND == file.split(".")[0] and file.split(".")[-1] != 'zip':
+            if self.BRAND == file.split(".")[0] and file.split(".")[-1] != "zip":
                 path_to_file = f'{self.PATH_TO_FILE}{self.BRAND}.{file.split(".")[-1]}'
                 files.append(path_to_file)
 
         for i in files:
             try:
-                logger.info('transfer file to ftp')
-                with open(path_to_file, "rb") as file:
-                    ftp_server.storbinary(f'STOR {os.path.basename(i)}', file)
-                    data = ftplib.FTP.retrlines(ftp_server, 'LIST')
+                logger.info("transfer file to ftp")
+                with open(i, "rb") as file:
+                    ftp_server.storbinary(f"STOR {os.path.basename(i)}", file)
+                    data = ftplib.FTP.retrlines(ftp_server, "LIST")
                     logger.info(data)
             except Exception as ex:
-                logger.error(f'{self.BRAND}: {ex}')
+                logger.error(f"{self.BRAND}: {ex}")
 
         ftp_server.quit()
