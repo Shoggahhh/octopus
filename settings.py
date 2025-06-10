39c86dc3c560c94b6e5304f77e851b1cc27cacde
vasiliy_grinev <vass_stn@mail.ru>
2025-06-10 14:27:51 +0300

    fix settings.py

--- settings.py
+++ settings.py
@@ -10,13 +10,18 @@ class OctopusSettings(BaseSettings):
     ftp_user: str = "ftp_user"
     ftp_password: str = "ftp_password"
     ftp_path: str = "ftp_path"
-    path_to_file: str = BASE_DIR / "files"
+    path_to_file: Path = BASE_DIR / "files"
+
+    imap_host: str = "imap_host"
+    imap_user: str = "imap_user"
+    imap_pass: str = "imap_pass"
+    folder: str = "folder"
 
     model_config = SettingsConfigDict(env_file=ENV_FILE, extra="ignore")
 
 
 class Settings(BaseSettings):
-    octopus_settings = OctopusSettings()
+    octopus_settings: OctopusSettings = OctopusSettings()
 
 
 settings = Settings()
