97b5ccbdecbaeeaed9d9e93f349c5920920ef832
vasiliy_grinev <vass_stn@mail.ru>
2025-06-10 18:32:53 +0300

    review logger.py

--- logger.py
+++ logger.py
@@ -10,10 +10,20 @@ FILE_DIR.mkdir(exist_ok=True)
 FORMAT = "%(asctime)s %(levelname)s %(name)s %(message)s"
 LOG_FILE = LOGS_DIR / "logs.log"
 
-logging.basicConfig(
-    format=FORMAT,
-    filename=LOG_FILE,
-    level=logging.INFO,
-    encoding="utf-8",
-)
+logging.getLogger("exchangelib.fields").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(FORMAT)
+
+file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+logger.propagate = False
