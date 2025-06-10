be6bd0fe590b45e4e8aa516a2dad2f58dd29f977
vasiliy_grinev <vass_stn@mail.ru>
2025-06-10 18:33:22 +0300

    review main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from octopus import Octopus
-from pathlib import Path
 
 
 if __name__ == "__main__":
-    path_to_file = Path("files")
-    path_to_logs = Path("logs")
-    path_to_file.mkdir(exist_ok=True)
-    path_to_logs.mkdir(exist_ok=True)
 
     """
     Example:
-    
+
     some_brand = Octopus("Some_brand")
     some_brand.get_file_from_yandex(
         "url=yandex_url",
