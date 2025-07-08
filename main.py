f2e59a8b6d2ca369e2a81c753552a6aca679024e
vasiliy_grinev <vass_stn@mail.ru>
2025-07-08 13:22:44 +0300

    change main docstring

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from octopus import Octopus
-
+from settings import settings
+from download_services import (
+    DownloaderFromYandex,
+    DownloaderFromGoogle,
+    DownloaderFromMailRu,
+    DownloaderFromLinkAuth,
+    DownloaderFromLink,
+    DownloaderFromOutlookExchange,
+)
+from file_service import FileDownloader
+from ftp_service import ftp_client
 
 if __name__ == "__main__":
 
     """
     Example:
 
-    some_brand = Octopus("Some_brand")
-    some_brand.get_file_from_yandex(
-        "url=yandex_url",
-        "file_name=file_name_from_yandex_disk",
-        format_file_xlsx="xlsx",
-    )
+    my_brand = DownloaderFromYandex("my_brand")
+    my_brand.download("some_url", "file_name", "xls")
+
     """
+    my_brand = DownloaderFromYandex("my_brand")
+    my_brand.download("some_url", "file_name", "xls")
+    ftp_client.transfer_to_ftp()
